11b6bb26a25cdb474c9a0373a5fe1d8d5219ecd0
cozzza <julietacozza@gmail.com>
2024-11-19 16:46:27 -0300

    audios actualizados loopeados

--- src/VideoSelector.js
+++ src/VideoSelector.js
@@ -1,27 +1,40 @@
 import { useState, useRef, useEffect } from 'react';
-import { frames, audios } from './arrays';
+import { frames } from './arrays';
 import './styles.css';
 import PlayCircleOutlineIcon from '@mui/icons-material/PlayCircleOutline';
 import toast, { Toaster } from 'react-hot-toast';
-import { Height } from '@mui/icons-material';
 
 export const VideoSelector = () => {
   const [audioList, setAudioList] = useState([]);
   const [isPlaying, setIsPlaying] = useState(false);
   const [currentAudioIndex, setCurrentAudioIndex] = useState(0);
+  const [playCounts, setPlayCounts] = useState({});
   const audioRef = useRef(new Audio());
 
   const notify = () => toast('Audio agregado a la lista');
 
   const handleAddToQueue = (index) => {
-    setAudioList((prev) => [...prev, index]);
-    notify()
+    setPlayCounts((prevCounts) => {
+      const currentCount = prevCounts[index] || 0;
+      const nextAudioIndex = currentCount < frames[index].audios.length - 1 ? currentCount : 1;
+
+      setAudioList((prev) => [
+        ...prev,
+        { frameIndex: index, audioSrc: frames[index].audios[nextAudioIndex] },
+      ]);
+
+      notify();
+      return {
+        ...prevCounts,
+        [index]: currentCount + 1,
+      };
+    });
   };
 
   const playAudioQueue = () => {
     if (audioList.length > 0 && currentAudioIndex < audioList.length) {
-      const audioKey = Object.keys(audios)[audioList[currentAudioIndex]];
-      audioRef.current.src = audios[audioKey];
+      const { audioSrc } = audioList[currentAudioIndex];
+      audioRef.current.src = audioSrc;
       audioRef.current.play();
       setIsPlaying(true);
 
@@ -34,7 +47,7 @@ export const VideoSelector = () => {
       setCurrentAudioIndex(0);
     }
   };
-  
+
   useEffect(() => {
     if (!isPlaying && currentAudioIndex < audioList.length) {
       playAudioQueue();
@@ -44,27 +57,28 @@ export const VideoSelector = () => {
   return (
     <div>
       <div>
-        <h1 style={{ color: 'white', fontFamily:'sans-serif'}}>Un cuerpo expuesto</h1>
-        <h3 style={{ color: 'white', fontFamily:'sans-serif' }}>Selecciona un sonido</h3>
+        <h1 style={{ color: 'white', fontFamily: 'sans-serif' }}>Un cuerpo expuesto</h1>
+        <h3 style={{ color: 'white', fontFamily: 'sans-serif' }}>Selecciona un sonido</h3>
       </div>
       <div className="video-grid">
         {frames.map((frame, index) => (
           <div key={index} className="video-card" onClick={() => handleAddToQueue(index)}>
-            <img src={frame} alt={`Frame ${index}`} />
-            <div >
-            {audioList[currentAudioIndex] === index && isPlaying ? (
-              <div className="equalizer-bar-container">
-                <div className="equalizer-bar"></div>
-                <div className="equalizer-bar"></div>
-                <div className="equalizer-bar"></div>
-              </div>
+            <img src={frame.image} alt={`Frame ${frame.id}`} />
+            <div>
+              {audioList[currentAudioIndex]?.frameIndex === index && isPlaying ? (
+                <div className="equalizer-bar-container">
+                  <div className="equalizer-bar"></div>
+                  <div className="equalizer-bar"></div>
+                  <div className="equalizer-bar"></div>
+                </div>
               ) : (
-                <PlayCircleOutlineIcon fontSize="large" className="icon-overlay" /> )} 
-                <Toaster
-                  toastOptions={{ style: {fontFamily: 'sans-serif'} }}
-                  position="bottom-center"
-                  reverseOrder={false}
-                />
+                <PlayCircleOutlineIcon fontSize="large" className="icon-overlay" />
+              )}
+              <Toaster
+                toastOptions={{ style: { fontFamily: 'sans-serif' } }}
+                position="bottom-center"
+                reverseOrder={false}
+              />
             </div>
           </div>
         ))}
--- src/arrays.js
+++ src/arrays.js
@@ -1,22 +1,4 @@
-import abdominales from './frames/abdominales.png'
-import mandibula from './frames/mandibula.png';
-import pelota from './frames/pelota.png';
-import tabaco from './frames/tabaco.png';
-import makeup from './frames/makeup.png';
-import comoPosar from './frames/comoPosar.png';
-import aplaudir_1 from './frames/aplaudir_1.png'
-import saltar_2 from './frames/saltar_2.png';
-
-import abdominales_1 from './AUDIOS/abdominales_1.mp3';
-import mandibula1 from './AUDIOS/mandibula1.mp3';
-import jueguitos from './AUDIOS/jueguitos.mp3';
-import saltar_cuerda from './AUDIOS/saltar_cuerda.mp3';
-import emiliamakeup_2 from './AUDIOS/emiliamakeup_2.mp3';
-import cigarrillo from './AUDIOS/cigarrillo.mp3';
-import como_posar2 from './AUDIOS/como_posar2.mp3';
-import aplaudir from './AUDIOS/aplaudir.mp3';
-
-export const frames = [
+/* export const frames = [
   abdominales,
   mandibula,
   pelota,
@@ -29,11 +11,56 @@ export const frames = [
 
 export const audios = {
   abdominales_1,
+  abdominales_2,
   mandibula1,
+  mandibula2,
   jueguitos,
+  jueguitos2,
   cigarrillo,
+  cigarrillo2,
+  emiliamakeup_1,
   emiliamakeup_2,
+  como_posar1,
   como_posar2,
   saltar_cuerda,
-  aplaudir
-}
+  saltar_cuerda2,
+  aplaudir,
+  aplaudir2
+} */
+
+import abdominales from './frames/abdominales.png';
+import mandibula from './frames/mandibula.png';
+import pelota from './frames/pelota.png';
+import tabaco from './frames/tabaco.png';
+import makeup from './frames/makeup.png';
+import comoPosar from './frames/comoPosar.png';
+import aplaudir_1 from './frames/aplaudir_1.png';
+import saltar_2 from './frames/saltar_2.png';
+
+import abdominales1 from './AUDIOS/abdominales1.mp3';
+import abdominales2 from './AUDIOS/abdominales2.mp3';
+import mandibula1 from './AUDIOS/mandibula1.mp3';
+import mandibula2 from './AUDIOS/mandibula2.mp3';
+import jueguitos1 from './AUDIOS/jueguitos1.mp3';
+import jueguitos2 from './AUDIOS/jueguitos2.mp3';
+import saltar1 from './AUDIOS/saltar1.mp3';
+import saltar2 from './AUDIOS/saltar2.mp3';
+import makeup1 from './AUDIOS/makeup1.mp3';
+import makeup2 from './AUDIOS/makeup2.mp3';
+import cigarrillo1 from './AUDIOS/cigarrillo1.mp3';
+//import cigarrillo2 from './AUDIOS/cigarrillo2.mp3';
+import posar1 from './AUDIOS/posar1.mp3';
+import posar2 from './AUDIOS/posar2.mp3';
+import aplaudir1 from './AUDIOS/aplaudir1.mp3';
+import aplaudir2 from './AUDIOS/aplaudir2.mp3';
+
+export const frames = [
+  { id: 'abdominales', image: abdominales, audios: [abdominales1, abdominales2] },
+  { id: 'mandibula', image: mandibula, audios: [mandibula1, mandibula2] },
+  { id: 'tabaco', image: tabaco, audios: [cigarrillo1] },
+  { id: 'makeup', image: makeup, audios: [makeup1, makeup2] },
+  { id: 'comoPosar', image: comoPosar, audios: [posar1, posar2] },
+  { id: 'saltar', image: saltar_2, audios: [saltar1, saltar2] },
+  { id: 'aplaudir', image: aplaudir_1, audios: [aplaudir1, aplaudir2] },
+  { id: 'jueguitos', image: pelota, audios: [jueguitos1, jueguitos2]}
+];
